b945526e297a9e5b4379ac9e09b5097499cdf6a5
Sara McCauley <sara.hizar@outlook.com>
2024-02-22 16:08:04 -0600

    still deploy errors

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,12 +7,10 @@ module.exports = {
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
-    'react/jsx-no-target-blank': 'off',
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
